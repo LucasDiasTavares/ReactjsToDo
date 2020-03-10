ef6cd64cf15ffc6f38469a6e3ec562a74f76e16f
Tavares <lucas.tavares1996@hotmail.com>
2020-03-10 20:28:59 -0300

    Adicionando Bootstrap e começo do layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div id="task-container">
+                    <div id="form-wrapper">
+                        <form id="form">
+                            <div className="d-flex">
+                                <div style={{ flex: 6 }}>
+                                    <input className="form-control" id="title" type="text" name="title" placeholder="Adicionar tarefa" />
+                                </div>
+
+                                <div style={{ flex: 1 }}>
+                                    <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div id="list-wrapper">
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
